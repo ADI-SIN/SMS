3da758b787a948471f8f00bf13f3c5d1c23c6c5e
ADI-SIN <masteraditya11a@rediffmail.com>
2017-04-18 22:56:58 +0530

    second commit

--- app/src/main/java/com/example/adityasi/sms/MainActivity.java
+++ app/src/main/java/com/example/adityasi/sms/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.adityasi.sms;
 
 import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 phoneNo=txtphoneNo.getText().toString();
                 message=txtMessage.getText().toString();
 
-                sendSMSMessage(phoneNo,message);
+                sendSMS(phoneNo,message);
             }
         });
     }
@@ -79,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
     */
 
-    private void sendSMSMessage(String phoneNo,String message ){
+   /* private void sendSMSMessage(String phoneNo,String message ){
 
       try {
           SmsManager smsManager = SmsManager.getDefault();
@@ -95,5 +98,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }*/
+
+
+    private void sendSMS(String phoneNumber, String message)
+    {
+        Log.v("phoneNumber",phoneNumber);
+        Log.v("MEssage",message);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, ignore.class), 0);
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, pi, null);
     }
 }
---
+++ app/src/main/java/com/example/adityasi/sms/ignore.java
@@ -0,0 +1,8 @@
+package com.example.adityasi.sms;
+
+/**
+ * Created by ADITYA SI on 4/18/2017.
+ */
+
+public class ignore {
+}
